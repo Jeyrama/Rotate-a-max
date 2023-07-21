9f537e611439b24cf236612408d0c3b3f0f5f4d5
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-07-21 12:25:27 -0700

    Update Rotate-a-max.js

--- Rotate-a-max.js
+++ Rotate-a-max.js
@@ -3,15 +3,16 @@ Let us begin with an example:
 Take a number: 56789. Rotate left, you get 67895.
 Keep the first digit in place and rotate left the other digits: 68957.
 Keep the first two digits in place and rotate the other ones: 68579.
-Keep the first three digits and rotate left the rest: 68597. Now it is over since keeping the first four it remains only one digit which rotated is itself.
+Keep the first three digits and rotate left the rest: 68597. 
+Now it is over since keeping the first four it remains only one digit which rotated is itself.
 
 You have the following sequence of numbers:
 56789 -> 67895 -> 68957 -> 68579 -> 68597
 and you must return the greatest: 68957.
 
 Task
-Write function max_rot(n) which given a positive integer n returns the maximum number you got doing rotations similar to the above example.
-So max_rot (or maxRot or ... depending on the language) is such as:
+  Write function max_rot(n) which given a positive integer n returns the maximum number you got doing rotations similar to the above example.
+  So max_rot (or maxRot or ... depending on the language) is such as:
     max_rot(56789) should return 68957
     max_rot(38458215) should return 85821534
 */
@@ -20,9 +21,9 @@ So max_rot (or maxRot or ... depending on the language) is such as:
 // Solution
 
 function maxRot(n) {
-  var str = n.toString();
-  var arr = [str];
-  for (var i=0;i<=str.length-1 ;i++){
+  let str = n.toString();
+  let arr = [str];
+  for (let i=0;i<=str.length-1 ;i++){
       str = str.slice(0,i)+str.slice(i+1)+str[i];
       arr.push(str.split().join());
   }
@@ -33,19 +34,16 @@ function maxRot(n) {
 
 function maxRot(n) {
   //turn our number into an array
-    var num = ('' + n).split('');
+    let num = ('' + n).split('');
   //create holder array so that later we can get the highest number in the array
-    var holderArr = [n];
-  
+    let holderArr = [n];
   //loop through array
-    for(var i = 0; i < num.length -1; i++){
+    for(let i = 0; i < num.length -1; i++){
   //take whatever number is at i index of our num array and move it to the end until loop stops
       num.push(num.splice(i,1));
   //push our num to our holder array as an integer 
       holderArr.push(parseInt(num.join('')));
     }
-  
   //return the highest number in our holder array
     return Math.max.apply(num, holderArr);
-  
-  }
+}
